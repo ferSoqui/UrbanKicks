b816b8de172633ca945d27b5754ced01669d7326
cocopub <emma.sahagun@iest.edu.mx>
2026-05-08 15:22:08 -0600

    rotacion de pantalla funcional y ajustada a las vistas

--- app/src/main/java/com/example/urbankicks/vistas/CarritoView.kt
+++ app/src/main/java/com/example/urbankicks/vistas/CarritoView.kt
@@ -4,7 +4,9 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.ShoppingCart
@@ -49,7 +51,7 @@ fun CarritoView(navController: NavHostController, carrito: MutableList<ItemCarri
         modifier = Modifier.fillMaxSize(),
         color = FondoClaro
     ) {
-        Column(modifier = Modifier.fillMaxSize()) {
+        Column(modifier = Modifier.fillMaxSize().verticalScroll(rememberScrollState())) {
 
             Row(
                 modifier = Modifier
